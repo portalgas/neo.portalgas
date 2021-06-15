ae79c5edaf9fa608d38430d770b3494e56dfe5b1
fractis <francesco.actis@atlanticmoon.it>
2021-06-15 13:30:20 +0200

    order by code

--- src/Model/Table/ArticlesOrdersTable.php
+++ src/Model/Table/ArticlesOrdersTable.php
@@ -564,7 +564,7 @@ class ArticlesOrdersTable extends Table
     }    
 
     protected function _getOptions($options) {
-        isset($options['sort']) && !empty($options['sort']) ? $this->_sort = $options['sort']: $this->_sort = [$this->getAlias().'.name'];
+        isset($options['sort']) && !empty($options['sort']) ? $this->_sort = $options['sort']: $this->_sort = ['Articles.codice', $this->getAlias().'.name'];
         isset($options['limit']) && !empty($options['limit']) ? $this->_limit = $options['limit']: $this->_limit = Configure::read('sql.limit');
         isset($options['page']) && !empty($options['page']) ? $this->_page = $options['page']: $this->_page = 1; 
     }
