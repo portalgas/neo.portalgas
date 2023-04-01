519384a665a40db5371c52936ef16512329e0086
luca.trevisani246 <luca@galileo146.it>
2023-04-01 19:20:51 +0200

    bug organizationHasFieldCartNote

--- src/Model/Table/CashesUsersTable.php
+++ src/Model/Table/CashesUsersTable.php
@@ -249,7 +249,9 @@ class CashesUsersTable extends Table
 
         $results = [];
 
-        if(!isset($user->organization->paramsConfig) || $user->organization->paramsConfig['hasCashFilterSupplier']=='N')
+        if(!isset($user->organization->paramsConfig) || 
+            !isset($user->organization->paramsConfig['hasCashFilterSupplier']) ||
+            $user->organization->paramsConfig['hasCashFilterSupplier']=='N')
             return $results;
 
         $supplierOrganizationCashExcludedsTable = TableRegistry::get('SupplierOrganizationCashExcludeds');
