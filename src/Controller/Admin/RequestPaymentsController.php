3526e2cd2c262c4ed0693b395974004059bc10ed
factis <factis@dmatorino.it>
2025-03-07 14:00:31 +0100

    testo mail sollecito

--- src/Controller/Admin/RequestPaymentsController.php
+++ src/Controller/Admin/RequestPaymentsController.php
@@ -51,7 +51,7 @@ class RequestPaymentsController extends AppController
         $mail_subject = $this->_organization->name." - sollecito pagamento per la richiesta num°".$request_payment->num;
 
         $mail_body_pre = "Salve Mario Rossi,\r\nal momento non è ancora pervenuto il tuo pagamento relativo alla richiesta n°".$request_payment->num." inviata il ".$request_payment->data_send->format('d/m/Y').", ";
-        $mail_body = "in modo da permetterci di poter pagare i fornitori in tempo con gli impegni presi.\r\nSe hai già provveduto al pagamento inviaci una copia del pagamento effettuato così che possa inserirlo fra i saldati.";
+        $mail_body = "utile per pagare i fornitori in tempo con gli impegni presi.\r\nSe hai già provveduto al pagamento inviaci una copia del pagamento effettuato così che possa inserirlo fra i saldati.";
         $mail_body_post = "\r\nAl momento risulta da saldare 99,01 €.\r\nPer maggior dettagli Collegati al sito ".$portalgas_fe_url." e, dopo aver fatto la login, scarica il documento per effettuare il pagamento.\r\n\r\nSe effettui il pagamento tramite bonifico indica come causale: Richiesta num ".$request_payment->num." di Mario Rossi\r\nGrazie.";
 
         $this->set(compact('request_payment', 'mail_subject', 'mail_body_pre', 'mail_body', 'mail_body_post'));
